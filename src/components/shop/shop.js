5e7f7cb0ee4bec11502243c8bc6e68daf45acf7a
fultonbryson <dev.brysonfulton@gmail.com>
2021-12-30 19:24:20 -0700

    refactored navbar to take additional onClick

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -14,13 +14,19 @@ class Shop extends Component {
     ];
     this.props.setHeaderLinks(headerLinks);
     this.props.fetchShopCategories();
-
-    // fetch shop products action creator
-    // fetch navbar links
-    // set navbar
     // filter products with links
     this.props.fetchShopProducts();
   }
+
+  shouldComponentUpdate(nextProps) {
+    if (this.props != nextProps) {
+      this.props.setNavbarLinks(nextProps.categories, (_id) =>
+        this.props.filterProductsWithCategoryId(_id)
+      );
+    }
+    return true;
+  }
+
   render() {
     return (
       <div className='shop'>
@@ -35,7 +41,10 @@ class Shop extends Component {
 }
 
 function mapStateToProps(state) {
-  return { state };
+  const { categories } = state.shop;
+  return {
+    categories,
+  };
 }
 
 Shop = connect(mapStateToProps, actions)(Shop);
