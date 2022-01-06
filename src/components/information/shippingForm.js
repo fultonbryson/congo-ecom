867cf6bed1aead29fb52eef63005b987e614139a
fultonbryson <dev.brysonfulton@gmail.com>
2022-01-05 18:35:13 -0700

    built order summary component, info title and underlined title components

--- src/components/information/shippingForm.js
+++ src/components/information/shippingForm.js
@@ -6,6 +6,8 @@ import { FormInput, FormButton } from "../formFields";
 
 import history from "../../history";
 
+import OrderSummary from "./orderSummary";
+
 class ShippingForm extends Component {
   render() {
     const { className, handleSubmit } = this.props;
@@ -72,6 +74,7 @@ class ShippingForm extends Component {
           short={true}
           component={FormButton}
         />
+        <OrderSummary className='shipping-form__summary' />
       </form>
     );
   }
