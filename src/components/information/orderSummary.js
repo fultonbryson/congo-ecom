01d069c2624bc875fa8f00e0fc6afb9db6f0df72
fultonbryson <dev.brysonfulton@gmail.com>
2022-01-05 19:08:43 -0700

    finished shipping form component and subcomponents

--- src/components/information/orderSummary.js
+++ src/components/information/orderSummary.js
@@ -1,31 +1,49 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import { UnderlinedTitle, InfoTitle } from "./infoHelp";
 
 class OrderSummary extends Component {
   render() {
     const { className } = this.props;
+    let subtotal = 0;
+    let tax = 0.16;
+    let cartCount = 0;
+    this.props.cartProducts.map((cartProduct) => {
+      subtotal += cartProduct.quantity * cartProduct.product.price;
+      cartCount += cartProduct.quantity;
+    });
     return (
       <div className={`${className} order-summary`}>
-        <UnderlinedTitle className='order-summary__title' />
+        <UnderlinedTitle
+          className='order-summary__title'
+          title='Order Summary'
+        />
         <InfoTitle
           className='order-summary__subtotal'
-          title='4 Stickers'
-          value='$7.96'
+          title={`${cartCount} Stickers`}
+          value={`$${subtotal}`}
         />
         <InfoTitle
           className='order-summary__tax-shipping'
           title='Taxes & Shipping'
-          value='$0.00'
+          value={`$${tax}`}
         />
         <InfoTitle
-          className='order-summary__total'
+          className='order-summary__total info-title-green'
           title='Total'
-          value='$8.02'
+          value={`$${subtotal + tax}`}
         />
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  const { cartProducts } = state.user;
+  return { cartProducts };
+}
+
+OrderSummary = connect(mapStateToProps)(OrderSummary);
 
 export default OrderSummary;
